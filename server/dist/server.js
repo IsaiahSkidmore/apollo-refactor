295ea979751ab0d43e56f379b9002bad36cb25f4
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-12-08 15:20:46 -0800

    we'll see

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,33 +1,28 @@
 import express from 'express';
-import path from 'node:path';
-import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schema/index.js';
-import { fileURLToPath } from 'node:url';
-import { authenticateToken } from './services/auth-service.js';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from 'path';
+// import expressMidlleware from '@apollo/server-express';
+import { typeDefs, resolvers } from './schemas/index.js';
+import db from './config/connection.js';
+const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
 });
 const startApolloServer = async () => {
     await server.start();
-    await db;
-    const PORT = process.env.PORT || 3001;
-    const app = express();
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-    app.use('/graphql', expressMiddleware(server, {
-        context: authenticateToken
-    }));
+    // app.use('/graphql', expressMiddleware(server));
+    // if we're in production, serve client/dist as static assets
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../../client/dist')));
+        app.use(express.static(path.join(__dirname, '../client/dist')));
         app.get('*', (_req, res) => {
-            res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
         });
     }
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
